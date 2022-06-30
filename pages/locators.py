7413371b0c87b011e416ff953827bea9a57d056a
Андрей <andrey_pav@inbox.ru>
2022-06-30 22:27:48 +0300

    Реализован переход между страницами циклическим способом

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOG_IN = (By.CSS_SELECTOR, "[value='Log In']")
+    REGISTER = (By.CSS_SELECTOR, "[value='Register1']")
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -6,3 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    login_page=page.go_to_login_page()
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
+    login_page.should_be_login_url()
+    
