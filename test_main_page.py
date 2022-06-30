9b7e25791178faac08ed257e27cd4c59ae569d23
Андрей <andrey_pav@inbox.ru>
2022-06-30 20:49:39 +0300

    Тест преобразован в стиле Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,7 @@
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
-#тестовые данные
-link = "http://selenium1py.pythonanywhere.com/"
-
-# Открыть страницу логина (в отдельной функции)
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
-# Тест
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser,link)
+    page.open()
+    page.go_to_login_page()
