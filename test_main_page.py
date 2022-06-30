7b7f1590aaddf35a5cdaf167996dd591adc53682
Андрей <andrey_pav@inbox.ru>
2022-06-30 20:24:12 +0300

    Создан базовый класс открытия страницы BasePage()

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,14 @@
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+#тестовые данные
+link = "http://selenium1py.pythonanywhere.com/"
+
+# Открыть страницу логина (в отдельной функции)
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+# Тест
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
